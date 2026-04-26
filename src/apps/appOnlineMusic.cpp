fdfa8ccc32b36b4b0d8aaa807158edea2baa56b3
kanfandelong <2037443617@qq.com>
2026-04-27 03:52:35 +0800

    解决冲突

--- src/apps/appOnlineMusic.cpp
+++ src/apps/appOnlineMusic.cpp
@@ -143,10 +143,6 @@ void AppOnlineMusic::set()
 
 void AppOnlineMusic::setup()
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 5a82915a2253a7847298511701912ff188c21c25
     display.clearScreen();
     display.display();
 
@@ -202,21 +198,12 @@ void AppOnlineMusic::setup()
 
     while (!_end)
     {
-<<<<<<< HEAD
         if (isPlaying && mp3 && mp3->isRunning())
         {
             if (!mp3->loop())
             {
                 // 播放结束，切歌
                 stopSong();
-=======
-        if (mp3 && mp3->isRunning())
-        {
-            if (!mp3->loop())
-            {
-                mp3->stop();
-                isPlaying = false;
->>>>>>> 5a82915a2253a7847298511701912ff188c21c25
                 playNext();
                 showDisplay();
             }
@@ -886,11 +873,8 @@ void AppOnlineMusic::playSong(int index)
 
 void AppOnlineMusic::stopSong()
 {
-<<<<<<< HEAD
     isPlaying = false; // 先标记，这样主循环就不会再访问 mp3 了
 
-=======
->>>>>>> 5a82915a2253a7847298511701912ff188c21c25
     if (mp3)
     {
         mp3->stop();
@@ -903,10 +887,6 @@ void AppOnlineMusic::stopSong()
         delete httpStream;
         httpStream = nullptr;
     }
-<<<<<<< HEAD
-=======
-    isPlaying = false;
->>>>>>> 5a82915a2253a7847298511701912ff188c21c25
 }
 
 void AppOnlineMusic::showPlaylist(int page)
